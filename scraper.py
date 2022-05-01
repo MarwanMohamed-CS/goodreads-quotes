2f0424346a7c508e7cacd8e3687fa29d784e3e6a
Marwan Mohamed <marwan4544@hotmail.com>
2022-05-01 07:03:59 +0200

    n

--- scraper.py
+++ scraper.py
@@ -1,115 +1,64 @@
+from bs4 import BeautifulSoup
+from gtts import gTTS
+from matplotlib.pyplot import text
+import pandas as pd
 import requests
-from bs4 import BeautifulSoup, NavigableString
+import urllib.request
+import time
+import os
 
 
+class Quote():
+    def __init__(self, quote, author_name, book_name) -> None:
+        self.quote = quote
+        self.author_name = author_name
+        self.book_name = book_name
 
-def quotes_by_author(author, page_num=None):
-
-	old_author = author
-
-	author = author.replace(" ", "+")
-
-	all_quotes = []
-
-	# if page number not specified, get true page number
-	if page_num is None:
-		try:
-			page = requests.get("https://www.goodreads.com/quotes/search?commit=Search&page=1" + "&q=" + author + "&utf8=%E2%9C%93")
-			soup = BeautifulSoup(page.text, 'html.parser')
-			pages = soup.find(class_="smallText").text
-			a = pages.find("of ")
-			page_num = pages[a+3:]
-			page_num = page_num.replace(",", "").replace("\n", "")
-			page_num = int(page_num)
-			print("looking through", page_num, "pages")
-		except:
-			page_num = 1
-
-	# for each page
-	for i in range(1, page_num+1, 1):
-
-		try:
-			page = requests.get("https://www.goodreads.com/quotes/search?commit=Search&page=" + str(i) + "&q=" + author + "&utf8=%E2%9C%93")
-			soup = BeautifulSoup(page.text, 'html.parser')
-			print("scraping page", i)
-		except:
-			print("could not connect to goodreads")
-			break
-			
-		try:
-			quote = soup.find(class_="leftContainer")
-			quote_list = quote.find_all(class_="quoteDetails")
-		except:
-			pass
-
-		# get data for each quote
-		for quote in quote_list:
-
-			meta_data = []
-
-			# Get quote's text
-			try:
-				outer = quote.find(class_="quoteText")
-				inner_text = [element for element in outer if isinstance(element, NavigableString)]
-				inner_text = [x.replace("\n", "") for x in inner_text]
-				final_quote = "\n".join(inner_text[:-4])
-				meta_data.append(final_quote.strip())
-			except:
-				pass 
+    def format(self):
+        '''formats quotes object for audio to prounounce '''
+        return f'{self.quote}\n{self.author_name}{" " + self.book_name if self.book_name else ""}.\n' # note that the end point is to pause when reading 
+		# voice stops at stop points not lines 
 
 
-			# Get quote's author
-			try:
-				author = quote.find(class_="authorOrTitle").text
-				author = author.replace(",", "")
-				# author = author.replace("\n", "")
-				meta_data.append(author.strip())
-				# print(author)
-			except:
-				meta_data.append(None)
-
-			# Get quote's book title
-			try: 
-				title = quote.find(class_="authorOrTitle")
-				title = title.nextSibling.nextSibling.text
-				# title = title.replace("\n", "")
-				meta_data.append(title.strip())
-				# print(title)
-			except:
-				meta_data.append(None)
-
-			# Get quote's tags
-			try:
-				tags = quote.find(class_="greyText smallText left").text
-				tags = [x.strip() for x in tags.split(',')]
-				tags = tags[1:]
-				meta_data.append(tags)
-				# print(tags)
-			except:
-				meta_data.append(None)
-
-			# Get number of likes
-			try:
-				likes = quote.find(class_="right").text
-				likes = likes.replace("likes", "")
-				likes = int(likes)
-				meta_data.append(likes)
-				# print(likes)
-			except:
-				meta_data.append(None)
-
-			all_quotes.append(meta_data)
+def text_to_voice(text, text_lang='en', accent='co.uk', audio_file_name='text.mp3', Fast = True):
+    speech = gTTS(text, lang=text_lang, tld=accent, slow= not Fast)
+    # speech = gTTS(text = text, lang = language, slow = False)
+    speech.save(audio_file_name)
 
 
-		for text, author, title, tags, likes in all_quotes:
-			print(text)
-			print(author)
-			print(title)
-			print(tags)
-			print(likes)
-			print()
+def scrape_website(URL, page_number=''):
+    page_num = str(page_number)  # Convert the page number to a string
+    URL = URL + \
+        page_num  # append the page number to complete the URL
+    webpage = requests.get(URL)  # Make a request to the website
 
-	return all_quotes
+    # Create lists to store the scraped data
+    authors = []
+    quotes = []
+    # Parse the text from the website
+    soup = BeautifulSoup(webpage.text, "html.parser")
+    quoteText = soup.select('.quoteText')  # Get the tag and it's class
+    # quoteText = soup.find_all('div', attrs={'class':'quoteText'}) #Get the tag and it's class
+    for i in quoteText:
+        # Get the text of the current quote, but only the sentence before a new line
+        quote = i.text.strip().split('\n')[0]
+        author = i.find('span', attrs={'class': 'authorOrTitle'}).text.strip()
+        book_name = ''
+        try:
+            book_name = i.find(
+                'a', attrs={'class': 'authorOrTitle'}).text.strip()
+        except:
+            pass
+        quotes.append(Quote(quote, author, book_name))
+    return quotes
 
-quotes_by_author("jk rowling", 1)
 
+link = input('Link: ')
+if not link:
+    link = 'https://www.goodreads.com/author/quotes/3137322.Fyodor_Dostoevsky'
+
+quotes = scrape_website(link)
+
+quotes_text = ''.join(quotes)
+text_to_voice(quotes_text)
+os.system('start text.mp3')
